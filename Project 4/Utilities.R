2a7063998761ab4f1e8bb19ac6e37191fdbded50
BabyfaceKiller <55423884+BaybyfaceKiller@users.noreply.github.com>
2021-03-19 21:59:03 -0600

    Add files via upload

---
+++ Project 4/Utilities.R
@@ -0,0 +1,104 @@
+# Name: Chenxu Liu
+# SciNet username: tmp_cliu
+# Description:
+#  This script contains utility functions that perform actions on the
+#     dataset
+
+# Create a function that reads the csv file into a dataframe
+load_data <- function(file_name) {
+  # Read the csv file into a dataframe
+  data_df = read.csv(file_name)
+  # Return the dataframe
+  return(data_df)
+}
+
+# Create a function that calculates correlation indicators
+correlation_indicator <- function(df,exp_var,resp_var) {
+  cat("----------","\n")
+  cat("Computing correlation indicators...","\n")
+  # Calculate covariance of two variables
+  covariance <- var(df[,c(exp_var)],df[,c(resp_var)])
+  # Calcualte correlation of two variables
+  correlation <- cor(df[,c(exp_var)],df[,c(resp_var)])
+  # Print the covariance
+  cat("Covariance:",covariance,"\n")
+  # Print the correlation
+  cat("Correlation coefficient:",correlation,"\n")
+  # Return the result as a vector
+  return(c(covariance,correlation))
+}
+
+# Create a function that implements linear model
+linear_model <- function(df,exp_var,resp_var) {
+  cat("----------","\n")
+  cat("Fitting a Linear Model","\n")
+  # Fit linear model
+  model <- lm(df[,c(resp_var)] ~ df[,c(exp_var)])
+  # Print model summary
+  print(summary(model))
+  cat("----------","\n")
+  return(model)
+}
+
+# Create a function that implements quadratic model
+quadratic_model <- function(df,exp_var,resp_var) {
+  cat("----------","\n")
+  cat("Fitting a Quadratic Model","\n")
+  # Create the square of explanatory variable
+  exp_var2 <- df[,c(exp_var)]**2
+  # Fit quadratic model
+  model <- lm(df[,c(resp_var)] ~ df[,c(exp_var)]+exp_var2)
+  # Print model summary
+  print(summary(model))
+  cat("----------","\n")
+  return(model)
+}
+
+# Create a function that implements generalized linear model
+generalized_linear_model <- function(df,exp_var,resp_var) {
+  cat("----------","\n")
+  cat("Fitting a Generalized Linear Model","\n")
+  # Extract explanatory data
+  exp_data <- df[,c(exp_var)]
+  # Extract response data
+  resp_data <- df[,c(resp_var)]
+  # Fit generalized linear model
+  model <- glm(resp_data ~ exp_data,family=poisson(link='log'))
+  # Print model summary
+  print(summary(model))
+  cat("----------","\n")
+  return(model)
+}
+
+# Create a function that generates plot
+plot_function <- function(df,exp_var,resp_var,fitted_model,model_num) {
+  # If the model is quadratic
+  if (model_num == 2) {
+    # Plot datapoints
+    plot(df[,c(exp_var)],df[,c(resp_var)],xlab='Temperature',ylab='Count')
+    # Create a sequence of number in the same range of explanatory variable
+    xx <- seq(min(df[,c(exp_var)]), max(df[,c(exp_var)]),len = 100)
+    # Calculate the predicted value using the coefficents in the model
+    yy <- fitted_model$coef %*% rbind(1, xx, xx*xx)
+    # Plot the line representing the model
+    lines(xx, yy)
+    # If the model is linear
+  } else if (model_num == 1) {
+    # Plot datapoints
+    plot(df[,c(exp_var)],df[,c(resp_var)],xlab='Temperature',ylab='Count')
+    # Create a sequence of number in the same range of explanatory variable
+    xx <- seq(min(df[,c(exp_var)]), max(df[,c(exp_var)]),len = 100)
+    # Calcualte the predicted value using the coefficents in the model
+    yy <- fitted_model$coef %*% rbind(1, xx)
+    # Plot the line representing the model
+    lines(xx, yy)
+    # If the model is generalized linear
+  } else if (model_num == 3) {
+    # Plot datapoints
+    plot(df[,c(exp_var)],df[,c(resp_var)],xlab='Temperature',ylab='Count')
+    # Creat sorted explanatory variable
+    sorted_var <- sort(df[,c(exp_var)])
+    # Plot the line representing the model
+    lines(sorted_var,predict(fitted_model,data.frame(exp_data=sorted_var),type='response'))
+  }
+}
---
+++ Project 4/generateModels.R
@@ -0,0 +1,62 @@
+# Name: Chenxu Liu
+# SciNet username: tmp_cliu
+# Description:
+#  This script performs actions on the dataset using functions in the
+#     utility file based on given argument
+
+# Comment on observation of the result:
+#         (1) The result from linear model shows temperature has strong
+#             correlation with the number of bikes used
+#         (2) The result from quadratic model shows by adding the quadratic
+#             term to the model, the R-square improved and residual standard
+#             error decreased
+#         (3) Since the dependent variables are counts which are discrete numbers,
+#             I used poisson as error family and log as link, the plot shows
+#             generalized linear model is a better fit than quadratic model and
+#             the residual also dropped significantly
+
+# Source the utility file
+source('Utilities.R')
+# Get the argument from commandline
+args <- commandArgs(trailingOnly=TRUE)
+# Check if the length of commandline argument is 1
+if (length(args) != 1) {
+  cat("Error: This scripts requires only one argument: 1, 2 or 3","\n")
+  quit()
+}
+# If the commandline argument is 1
+if (args == 1) {
+  # Load the dataset
+  my_data <- load_data('day.csv')
+  # Calcualte correlation indicator between two specified variables
+  cor_ind=correlation_indicator(my_data,'temp','cnt')
+  # Fit a linear model with the data using specified variables
+  my_model <- linear_model(my_data,'temp','cnt')
+  # Plot the datapoints and the line for the model
+  plot_function(my_data,'temp','cnt',my_model,1)
+  # If the commandline argument is 2
+} else if (args == 2) {
+  # Load the dataset
+  my_data <- load_data('day.csv')
+  # Calcualte correlation indicator between two specified variables
+  cor_ind=correlation_indicator(my_data,'temp','cnt')
+  # Fit a quadratic model with the data using specified variables
+  my_model <- quadratic_model(my_data,'temp','cnt')
+  # Plot the datapoints and the line for the model
+  plot_function(my_data,'temp','cnt',my_model,2)
+  # If the commandline argument is 3
+} else if (args == 3) {
+  # Load the dataset
+  my_data <- load_data('day.csv')
+  # Calcualte the correlation indicator between two specified variables
+  cor_ind=correlation_indicator(my_data,'temp','cnt')
+  # Fit a generalized linear model with the data using specified variables
+  my_model <- generalized_linear_model(my_data,'temp','cnt')
+  # Plot the datapoints and the line for the model
+  plot_function(my_data,'temp','cnt',my_model,3)
+  # If the commandline argument is not as expected
+} else {
+  # Print the error message and quit
+  cat("Error: This scripts requires only one argument: 1, 2 or 3 ","\n")
+  quit()
+}
