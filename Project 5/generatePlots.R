f4a819afababf1a9182c72c6d78753cdef766cef
BabyfaceKiller <55423884+BaybyfaceKiller@users.noreply.github.com>
2021-03-19 22:01:52 -0600

    Add files via upload

---
+++ Project 5/generatePlots.R
@@ -0,0 +1,52 @@
+# Name: Chenxu Liu
+# SciNet username: tmp_cliu
+# Description:
+#  This script generates plots according to the commandline argument using
+#    functions in plottingTools.R
+
+
+# The data used to make plots is the age of the concrete in days and the strength
+#   of the concrete in MPa, the main purpose is to analyze if concrete with
+#   greater age will have less strength. The scatter plot is made of three parts:
+#   the scatter plot of Strength vs. Age, the density distribution of Age and
+#   the density distributio of Strength. Also, the color of the points in the
+#   scatter plot is related to the amount of cement used to build the concrete.
+#   These three plots are plotted on a blank plot to generate one single plot.
+#   Since the Age is a continuous variable, it was split into several boxes
+#   to generate the box plot. The dotted line indicates the average concrete
+#   strength in the dataset, the p-value of Kruskal test is
+#   shown on the left corner.
+
+# source utilities file
+source('plottingTools.R')
+# receive commandline argument
+args <- commandArgs(trailingOnly=TRUE)
+# if the length of commandline argument is not 1
+if (length(args) != 1) {
+  # print error message
+  cat("Accepted arguments: 'scatter' or 'box'.","\n")
+  # quit
+  quit()
+}
+
+# if the commandline argument is 'scatter'
+if (args=='scatter') {
+  # load the data
+  my_data=load_data('concrete_data.csv')
+  # create pdf to store the plot
+  pdf('scatter_plot.pdf')
+  # make scatter plot
+  scatter_plot(my_data)
+  # if the commandline argument is 'box'
+} else if (args=='box') {
+  # load the data
+  my_data=load_data('concrete_data.csv')
+  # create pdf to store the plot
+  pdf('box_plot.pdf')
+  # make box plot
+  box_plot(my_data)
+  # else print error message and quit
+} else {
+  cat("Accepted arguments: 'scatter' or 'box'.","\n")
+  quit()
+}
---
+++ Project 5/plottingTools.R
@@ -0,0 +1,55 @@
+# Name: Chenxu Liu
+# SciNet username: tmp_cliu
+# Description:
+#  This script contains functions that make scatter plot and box plot
+
+# Create a function that reads the csv file into a dataframe
+load_data <- function(file_name) {
+  # Read the csv file into a dataframe
+  data_df = read.csv(file_name)
+  # Return the dataframe
+  return(data_df)
+}
+
+# Create a function to make scatter plot
+scatter_plot <- function(df) {
+  # import libraries
+  library(ggplot2)
+  library(gridExtra)
+  # create scatter scatter plot
+  scatter<-ggplot(data=df,aes(x=Age,y=Strength))+geom_point(aes(color=Cement))+
+                  labs(x="Age(Days)",y="Strength(MPa)",color="Cement(kg)")
+
+  # create density plot for Age
+  age_density <- ggplot(data=df,aes(Age))+geom_density(alpha=0.5,fill='blue')+
+                 labs(x='Age(Days)',y='density')
+
+  # create density plot for Strength
+  strength_density <- ggplot(data=df,aes(Strength))+geom_density(alpha=0.5,
+                      fill='red')+labs(x='Strength(MPa)',y='density')+coord_flip()
+  # create a blank placeholder plot
+  blank_plot <- ggplot()+geom_blank(aes(1,1))+theme(plot.background=element_blank(),
+                 panel.grid.major=element_blank(),panel.grid.minor=element_blank(),
+                 panel.border=element_blank(),panel.background=element_blank(),
+                 axis.title.x = element_blank(),axis.title.y = element_blank(),
+                 axis.text.x = element_blank(),axis.text.y = element_blank(),
+                 axis.ticks = element_blank())
+  # plot the scatter plot and two density plots on the blank plot
+  grid.arrange(age_density, blank_plot, scatter, strength_density, ncol=2,
+               nrow=2, widths=c(5, 3), heights=c(2, 5))
+}
+
+# create a function to make box plot
+box_plot <- function(df) {
+  # import libraries
+  library(ggplot2)
+  library(ggpubr)
+  # extract the independent variable Age
+  ind_var <- df[,c('Age')]
+  # create bins for the continuous variable
+  bin=cut_width(ind_var,width=30,boundary=0)
+  # create boxplot using the created bins
+  ggplot(data=df,aes(x=bin,y=Strength))+geom_boxplot(aes(fill='#FF9999'),
+         show.legend=FALSE)+labs(x='Age(Days)',y='Strength(MPa)')+geom_hline(
+         yintercept=mean(df$Strength),linetype=2)+stat_compare_means()
+}
