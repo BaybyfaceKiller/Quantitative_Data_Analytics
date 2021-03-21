82a9fe2e5bfdf6563aa7efeb8196325b55ddc0db
BabyfaceKiller <55423884+BaybyfaceKiller@users.noreply.github.com>
2021-03-21 15:56:58 -0600

    Update evalMeans.sh

--- Project 1/evalMeans.sh
+++ Project 1/evalMeans.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 #evalMeans.sh
 
-# Name: Chenxu Liu
-# SciNet username: tmp_cliu
+
 # Description:
 #  This script calls selectMean.R twice with argument 0 and 1
 Rscript selectMean.R 0
