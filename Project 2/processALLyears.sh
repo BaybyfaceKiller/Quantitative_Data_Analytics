c487893fe7f0e884d51a2662501da72fdc9e94bf
BabyfaceKiller <55423884+BaybyfaceKiller@users.noreply.github.com>
2021-03-21 15:58:24 -0600

    Update processALLyears.sh

--- Project 2/processALLyears.sh
+++ Project 2/processALLyears.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 #processALLyears.sh
 
-# Name: Chenxu Liu
-# SciNet username: tmp_cliu
+
 # Description:
 #  This script loops over all years in the datafile and calls
     # process311.R each time
